b23d8f54e5ea54f745e690e2e2e8f1d255683ef7
ankitavashisth <ankitavashisth10@gmail.com>
2025-01-04 13:43:53 +0530

    First push

---
+++ src/test/java/com/example/shopoholics/ShopoholicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopoholics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopoholicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
